159f97ae081d5230b515f97191b36a2f71af4a5b
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2025-07-11 14:40:52 +0200

    Frontend only hosting bug fixing

--- data/placeholders/products.ts
+++ data/placeholders/products.ts
@@ -5,26 +5,26 @@ const products: ProductGroup[] = [
     id: "0",
     name: "Bottles",
     classic_variant_id: "0",
-    description: "public/placeholders/products/product-description/bottles.mdx"
+    description: "/placeholders/products/product-description/bottles.mdx"
   },
   {
     id: "1",
     name: "Eye masks",
     classic_variant_id: "1",
-    description: "public/placeholders/products/product-description/eye-masks.mdx",
+    description: "/placeholders/products/product-description/eye-masks.mdx",
     page_url: "/eye-masks"
   },
   {
     id: "2",
     name: "Make up pads",
     classic_variant_id: "2",
-    description: "public/placeholders/products/product-description/make-up-pads.mdx",
+    description: "/placeholders/products/product-description/make-up-pads.mdx",
   },
   {
     id: "3",
     name: "Reusable notebook",
     classic_variant_id: "3",
-    description: "public/placeholders/products/product-description/reusable-notebook.mdx",
+    description: "/placeholders/products/product-description/reusable-notebook.mdx",
     page_url: "/reusable-notebook"
   }
 ];
